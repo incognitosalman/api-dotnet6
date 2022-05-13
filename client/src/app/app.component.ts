be5f2522897d7320724fede77879a5513b629b6a
salman-develop <salman-dev@outlook.com>
2022-05-13 21:27:51 +0500

    End of Section 7

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/products?pageSize=50")
+      .subscribe((response: IPagination) => {
+        console.log(response);
+        this.products = response.data;
+      }, error => {
+        console.error(error);
+      });
+  }
+}
